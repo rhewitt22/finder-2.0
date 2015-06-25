3c47cfbb8e8651757b9a7fb7872b106a9899b620
Roy Hewitt <royhewitt07@gmail.com>
2015-06-25 16:36:59 -0400

    Enabled user profile update on frontend

--- frontend/app/scripts/controllers/profile.js
+++ frontend/app/scripts/controllers/profile.js
@@ -9,8 +9,23 @@
  */
 angular.module('frontendApp')
   .controller('ProfileCtrl', function ($scope, $http, toastr) {
+    $scope.update = function() {
+      var user = {
+        email: $scope.user.email,
+        job: $scope.user.job,
+        phone: $scope.user.phone,
+        accountType: $scope.user.accountType
+      };
+
+      $http.post('http://localhost:1337/user/update', user).then(function(response) {
+        console.log(response);
+        toastr.success('User updated');
+      }).catch(function(response) {
+        toastr.error(response.message, { message: 'Couldn\'t update user.' });
+      });
+    };
+
     $http.get('http://localhost:1337/user/profile').then(function(response) {
-      console.log(response.data);
       $scope.user = response.data;
     }).catch(function(response) {
       toastr.error(response.message, 'Couldn\'t get user history');
