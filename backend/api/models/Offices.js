14965320cc7b7f1e13e1a2bf2aafdcdfac08cf66
Roy Hewitt <royhewitt07@gmail.com>
2015-06-26 14:31:25 -0400

    Added offices API

---
+++ backend/api/models/Offices.js
@@ -0,0 +1,44 @@
+/**
+* Offices.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    name: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+
+    street: {
+      type: 'string',
+      required: true
+    },
+
+    city: {
+      type: 'string',
+      required: true
+    },
+
+    state: {
+      type: 'string',
+      required: true
+    },
+
+    zip: {
+      type: 'string',
+      required: true
+    },
+
+    phone: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
